c80658214546f4b82d7144fdafe6c9041d20da05
carlaight <charlesmix23@gmail.com>
2019-01-31 23:19:23 +0100

    updates || seeds pb city must exist

--- app/models/tag.rb
+++ app/models/tag.rb
@@ -1,6 +1,6 @@
 class Tag < ApplicationRecord
-  #belongs_to :user, optional: false # un seul user par tag
-  #belongs_to :gossip, optional: false # N'existe pas sans gossip
+  belongs_to :user, optional: false # un seul user par tag
+  belongs_to :gossip, optional: false # N'existe pas sans gossip
   has_many :join_table_tag_gossips
   has_many :gossips, :through => :join_table_tag_gossips
 end
--- db/seeds.rb
+++ db/seeds.rb
@@ -5,24 +5,34 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+# DELETE ALL
+City.destroy_all
+Gossip.destroy_all
+Tag.destroy_all
+User.destroy_all
+
+ # Création de 10 instances de la class Createcity afin de rajouter 10 city dans le tableau cities
+ 10.times do |index|
+  city = City.create!(name: Faker::Address.city, zipcode: Faker::Address.zip_code)
+  end
+
 
 # Création de 10 instances de la class CreateUser afin de rajouter 10 users dans le tableau users
 10.times do |index|
-  user = User.create!(first_name:Faker::Name.first_name, last_name: Faker::Name.last_name, city: City.all.sample,  age: rand(18..100), city_id: City.all.sample.id)
+  user = User.create!(first_name:Faker::Name.first_name, last_name: Faker::Name.last_name, description: 
+    Faker::GameOfThrones.quote, email: Faker::Internet.email, age: rand(18..100))
   end
 
-  # Création de 10 instances de la class Createcity afin de rajouter 10 city dans le tableau cities
-10.times do |index|
-  city = City.create!(name: Faker::Address.city)
-  end
+
   
 # Création de 20 instances de la class CreateGossip afin de rajouter 20 gossips dans le tableau gossips
 20.times do |index|
-  gossip = Gossip.create(title: Faker::Coffee.blend_name, content: Faker::HarryPotter.quote, user: User.all.sample)
-  
+  gossip = Gossip.create!(title: Faker::Coffee.blend_name, content: Faker::HarryPotter.quote, user_id: User.all.sample.id)
+end
 
 # Création de 10 instances de la class Createtag afin de rajouter 10 gossips dans le tableau tags
   10.times do |index|
-    tag = Tag.create(content: Faker::HarryPotter.spell, user: User.all.sample, )
-  
+    tag = Tag.create!(content: Faker::HarryPotter.spell, gossip_id: Gossip.all.sample.id)
+  end
+
 # PRIVATE MESSAGE
